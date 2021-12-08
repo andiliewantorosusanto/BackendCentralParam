881642f7ceb78a871e38f421b0a573beb7ff5b7a
andiliewantorosusanto <andiliesusanto@gmail.com>
2021-12-08 09:04:43 +0700

    changes

--- src/main/java/com/beCMS/BackendCentralParam/api/TipeKonsumenRestController.java
+++ src/main/java/com/beCMS/BackendCentralParam/api/TipeKonsumenRestController.java
@@ -1,12 +1,10 @@
 package com.beCMS.BackendCentralParam.api;
 
 import java.security.Principal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.jar.JarException;
+import java.util.Optional;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,17 +13,17 @@ import com.beCMS.BackendCentralParam.model.userlogin.User;
 import com.beCMS.BackendCentralParam.repository.TipeKonsumenRepository;
 import com.beCMS.BackendCentralParam.repository.UserRepository;
 
-import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,25 +39,47 @@ public class TipeKonsumenRestController {
     @Autowired
     private TipeKonsumenRepository tipeKonsumenRepository;
 
-    // NOTE Untuk liat list history penggunaan oleh Showroom REJECTED
     @PostMapping("/getalldata")
     public Map<String, Object> getAllDataTipeKonsumen(Principal principal, Pageable pageable,
             HttpServletResponse response) {
-        Map crunchifyMap = new HashMap<String, Object>();
-
-        // NOTE ambil dari token siapa yang sedang akses
-        System.out.println(principal.getName());
+        Map<String, Object> crunchifyMap = new HashMap<String, Object>();
         String id = principal.getName();
 
-        // NOTE cek diquery rolenya dia sebagai apa
         String role = userRepository.cekRoles(id);
         logger.info("NIP : " + id);
         logger.info("ROLE : " + role);
-        // NOTE kondisi dimana dia memiliki akses maka akan dilanjut
         if (role.contains("USER")) {
             try {
                 logger.info("Berhasil GET ALL DATA Tipe Konsumen");
                 crunchifyMap.put("dataTipeKonsumen", tipeKonsumenRepository.findAll());
+                crunchifyMap.put("code", "1");
+            } catch (Exception e) {
+                logger.error("ERROR");
+                response.setStatus(400);
+                crunchifyMap.put("code", "0");
+                crunchifyMap.put("message", "Gagal membuka Tipe Konsumen!");
+            }
+            return crunchifyMap;
+        } else {
+            logger.warn("TIDAK MEMILIKI HAK AKSES");
+            response.setStatus(400);
+            crunchifyMap.put("code", "00");
+            crunchifyMap.put("message", "OOPS. SOMETHING WENT WRONG !");
+            return crunchifyMap;
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Map<String, Object> getTipeKonsumen(Principal principal,@PathVariable Integer id,
+            HttpServletResponse response) {
+        Map<String, Object> crunchifyMap = new HashMap<String, Object>();
+        String userId = principal.getName();
+
+        String role = userRepository.cekRoles(userId);
+        if (role.contains("USER")) {
+            try {
+                logger.info("Berhasil Tipe Konsumen");
+                crunchifyMap.put("tipeKonsumen", tipeKonsumenRepository.findById(id));
                 crunchifyMap.put("code", "1");
             } catch (Exception e) {
                 logger.error("ERROR");
@@ -99,34 +119,39 @@ public class TipeKonsumenRestController {
         
         HashMap<String, String> crunchifyMap = new HashMap<>();
         crunchifyMap.put("code", "1");
-        crunchifyMap.put("message", "Input Tipe Konsumen Berhasil !");
+        crunchifyMap.put("message", "Input Dan Submit Tipe Konsumen Berhasil !");
         return crunchifyMap;
     }
 
-    @PostMapping(path = "/edit", consumes = "application/json")
-    public HashMap<String, String> editTipeKonsumen(@RequestBody modelTipeKonsumen modelTipeKonsumen,Principal principal) {
+    @PostMapping(path = "/delete", consumes = "application/json")
+    public HashMap<String, String> deleteTipeKonsumen(@RequestParam("id[]") List<Integer> ids,Principal principal) {
+        for(Integer id : ids) {
+            Optional<modelTipeKonsumen> opTionalModelTipeKonsumen = tipeKonsumenRepository.findById(id);
+            modelTipeKonsumen modelTipeKonsumen = opTionalModelTipeKonsumen.get();
+            tipeKonsumenRepository.delete(modelTipeKonsumen);
+        }
         
-        User user = userRepository.findBynip(principal.getName());
-        modelTipeKonsumen.trace(user.getId());
-
-        tipeKonsumenRepository.save(modelTipeKonsumen);
         HashMap<String, String> crunchifyMap = new HashMap<>();
         crunchifyMap.put("code", "1");
-        crunchifyMap.put("message", "Input Tipe Konsumen Berhasil !");
+        crunchifyMap.put("message", "Delete Tipe Konsumen Berhasil !");
         return crunchifyMap;
     }
 
-    @PostMapping(path = "/editAndSubmit", consumes = "application/json")
-    public HashMap<String, String> editAndSubmitTipeKonsumen(@RequestBody modelTipeKonsumen modelTipeKonsumen,Principal principal) {
+    @PostMapping(path = "/submit", consumes = "application/json")
+    public HashMap<String, String> submitTipeKonsumen(@RequestParam("id[]") List<Integer> ids,Principal principal) {
         
         User user = userRepository.findBynip(principal.getName());
-        modelTipeKonsumen.submit(user.getId());
 
-        tipeKonsumenRepository.save(modelTipeKonsumen);
+        for(Integer id : ids) {
+            Optional<modelTipeKonsumen> opTionalModelTipeKonsumen = tipeKonsumenRepository.findById(id);
+            modelTipeKonsumen modelTipeKonsumen = opTionalModelTipeKonsumen.get();
+            modelTipeKonsumen.submit(user.getId());
+            tipeKonsumenRepository.save(modelTipeKonsumen);
+        }
         
         HashMap<String, String> crunchifyMap = new HashMap<>();
         crunchifyMap.put("code", "1");
-        crunchifyMap.put("message", "Input Tipe Konsumen Berhasil !");
+        crunchifyMap.put("message", "Edit Dan Submit Tipe Konsumen Berhasil !");
         return crunchifyMap;
     }
 }
